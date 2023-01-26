a7ef67be3541612e819a7d01ec091c69ddab21cd
PopovDS617 <popovds.bm@gmail.com>
2023-01-27 00:29:18 +0300

    chore: ready to deploy

--- routes/characters.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const characters_1 = require("../controllers/characters");
-const router = (0, express_1.Router)();
-router.route('/characters').get(characters_1.getCharacters);
-exports.default = router;
